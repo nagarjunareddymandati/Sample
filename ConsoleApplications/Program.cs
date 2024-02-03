01eed45a2d7ee30d28f3f61cf75c314d1dd741c1
nag <nag@123>
2024-02-03 10:34:14 +0530

    first comit

--- ConsoleApplications/Program.cs
+++ ConsoleApplications/Program.cs
@@ -10,6 +10,32 @@ namespace ConsoleApplications
     {
         static void Main(string[] args)
         {
+            int[] ar = new int[] { 12, 43, 21, 65, 42, 22, 11, 4, 9, 98, 54 };
+            int size = 0;
+            for(int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] % 2 == 0)
+                {
+                    size++;
+                }
+            }
+            int[] br = new int[size];
+            int index = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] % 2 == 0)
+                {
+                    br[index] = ar[i];
+                    index++;
+                }
+            }
+            foreach (var item in br)
+            {
+                Console.Write(item + " ");
+
+                
+
+            }
         }
     }
 }
